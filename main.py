0a0775901dd23c7bf076f8866e8dd866e4befab7
datarec <73265375+datarec@users.noreply.github.com>
2025-02-11 19:52:39 +1100

    Create main.py
    added main files.

---
+++ main.py
@@ -0,0 +1,22 @@
+  GNU nano 8.2                                                               test.py                                                                        import time
+import math
+
+
+def main():
+    try:
+        byte_size = int(input("enter hard drive space: "))
+        exponent = math.exp2(40) * byte_size
+        confirm_attack = input("press y/n to confirm: ")
+        if (confirm_attack == "y"):
+            print("starting attack.")
+            junk_data = "a" * (exponent)
+            with open("log1.bin", "w") as f:
+                f.write(junk_data)
+            print("operation successful!")
+        elif (confirm_attack == "n"):
+            print("exiting...")
+            exit()
+    except TypeError:
+        print("ERROR: please enter size in gb for hard-drive.")
+
+main()
